3ebba6c3656e6c8ead824f70595006116e010dd5
Icecream <981464828@qq.com>
2019-01-22 11:37:16 +0800

    add files

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package src
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	startIDService()
+}
+
+// start http service of id generator
+func startIDService() {
+	idService := &http.Server{Addr: ":9999"}
+	http.HandleFunc("/", idServiceResponse) // 设置访问的路由
+
+	err := idService.ListenAndServe()
+	if err != nil {
+		log.Fatal("StartIDService: ", err)
+	}
+}
+
+// Response
+func idServiceResponse(w http.ResponseWriter, r *http.Request) {
+	id := GetIDInstance().NextID()
+	fmt.Fprintf(w, "%d", id) // 这个写入到w的是输出到客户端的
+}
